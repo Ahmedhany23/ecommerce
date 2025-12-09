96427e12a30192e19d0421ff0ec71a4946ab3d39
ahmedhany23 <ahmedhany.22@hotmail.com>
2025-12-10 00:48:32 +0200

    fix build

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
-
-    "/*": ["./node_modules/.prisma/client/**/*"],
-  },
 };
 
 export default nextConfig;
