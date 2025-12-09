a895fe8663d62d56fe9653ca3b18e693be58c858
ahmedhany23 <ahmedhany.22@hotmail.com>
2025-12-10 01:08:20 +0200

    fix: build error

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "./src/prisma/schema.prisma",
   migrations: {
-    path: "./src/prisma/migrations",
+    path: "/prisma/migrations",
   },
   engine: "classic",
   datasource: {
