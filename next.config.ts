afb0d1a149549ab03b44f3cb0e11bc77947ea925
ahmedhany23 <ahmedhany.22@hotmail.com>
2025-12-10 00:34:21 +0200

    fix prisma

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -13,12 +12,6 @@ const nextConfig: NextConfig = {
         hostname: "placehold.co",
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
   },
   outputFileTracingIncludes: {
     "/api/**/*": ["./node_modules/.prisma/client/**/*"],
